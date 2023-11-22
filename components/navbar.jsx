a57e227739fc829af492eb1db3c0cf45fe83e930
jaskaran-sethy <jase59354@gmail.com>
2023-11-23 00:40:53 +0530

    hotfix: mobile responsive navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import { useRouter } from 'next/router';
 import style from "./navbar.module.css";
 
 export default function NavBar() {
+    const router = useRouter();
     return (
         <div className={style.navbar}>
-            <Link href="/">Home</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/about">About</Link>
+            <Link href="/" className={router.pathname === '/' ? style.activeLink : ''}>Home</Link>
+            <Link href="/blog" className={router.pathname === '/blog' ? style.activeLink : ''}>Blog</Link>
+            <Link href="/about" className={router.pathname === '/about' ? style.activeLink : ''}>About</Link>
         </div>
     );
 }
