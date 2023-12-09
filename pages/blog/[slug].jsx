ed677c5183c2ebc2c57bf6149f1e58b4073b734d
jaskaran-sethy <jase59354@gmail.com>
2023-12-09 08:17:32 +0530

    feat: improve blog return visibility

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -44,16 +44,23 @@ export default function BlogPost() {
 
 function ReturnBar() {
     return (
-        <div className="returnBar" style={{position: "fixed", width: "10vw"}}>
+        <div className="returnBar" style={{position: "fixed", width: "10vw", zIndex: 1}}>
             <Link href="/blog" style={{ textDecoration: 'none' }}>
                 <div className="returnIcon" style={{width: "8vw", maxWidth:"64px"}}>
                     <svg viewBox="-3 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                        <defs>
+                            <linearGradient id="gradient">
+                                <stop offset="43%" stop-color="#0064c8" />
+                                <stop offset="100%" stop-color="#ff00b4" />
+
+                            </linearGradient>
+                        </defs>
                         <g id="icomoon-ignore">
                         </g>
-                        <path d="M13.114 2.887c-7.243 0-13.114 5.871-13.114 13.113s5.871 13.113 13.114 13.113c7.242 0 13.112-5.871 13.112-13.113s-5.87-13.113-13.112-13.113zM13.114 28.064c-6.653 0-12.065-5.412-12.065-12.064s5.412-12.063 12.065-12.063c6.652 0 12.063 5.412 12.063 12.063s-5.411 12.064-12.063 12.064z" fill="#eeeeee">
+                        <path d="M13.114 2.887c-7.243 0-13.114 5.871-13.114 13.113s5.871 13.113 13.114 13.113c7.242 0 13.112-5.871 13.112-13.113s-5.87-13.113-13.112-13.113zM13.114 28.064c-6.653 0-12.065-5.412-12.065-12.064s5.412-12.063 12.065-12.063c6.652 0 12.063 5.412 12.063 12.063s-5.411 12.064-12.063 12.064z" fill="url(#gradient)">
 
                         </path>
-                        <path d="M12.318 10.363l-0.742-0.742-6.379 6.379 6.379 6.379 0.742-0.742-5.113-5.113h12.726v-1.049h-12.726z" fill="#eeeeee">
+                        <path d="M12.318 10.363l-0.742-0.742-6.379 6.379 6.379 6.379 0.742-0.742-5.113-5.113h12.726v-1.049h-12.726z" fill="url(#gradient)">
 
                         </path>
                     </svg>
