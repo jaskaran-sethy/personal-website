9baac73b986c219ca495b8555f3ac40e645f04cf
jaskaran-sethy <jase59354@gmail.com>
2023-12-10 21:03:11 +0530

    feature: add victory for tic tac toe

--- pages/games/index.jsx
+++ pages/games/index.jsx
@@ -37,9 +37,16 @@ function TicTacToe() {
 
     function handleBoxClick(index) {
         let currState = gameState;
+        if (checkTicTacToeVictory(gameState)) {
+            return;
+        }
         if (currState[index] !== " ") return;
         currState = replaceCharacterAtIndex(currState, index, turn);
         setGameState(currState);
+        if (checkTicTacToeVictory(currState)) {
+            return;
+        }
+
         if (turn === "X") setTurn("O");
         else setTurn("X");
     }
@@ -54,6 +61,7 @@ function TicTacToe() {
             <div style={{display: "grid", gridTemplate: "repeat(3, 3rem) / repeat(3, 3rem)", gap: "0.3rem"}}>
                 {Boxes}
             </div>
+            {checkTicTacToeVictory(gameState) && <div style={{paddingTop: "1rem", textAlign: "center"}}>"{checkTicTacToeVictory(gameState)}" Won!!</div>}
             <div onClick={handleReset} style={{position: "relative", textAlign: "center", top: "1rem", background: "#eeeeee", color: "#111111", cursor: "pointer"}}>Reset</div>
         </>
     )
@@ -72,4 +80,48 @@ function replaceCharacterAtIndex(inputString, index, newCharacter) {
     const stringArray = inputString.split('');
     stringArray[index] = newCharacter;
     return stringArray.join('');
+}
+
+function checkTicTacToeVictory(gameState) {
+    if (gameState[0] === gameState[1] && gameState[0] === gameState[2]) {
+        if (gameState[0] === "X")
+            return "X";
+        else if (gameState[0] === "O") return "O";
+    }
+    if (gameState[0] === gameState[3] && gameState[0] === gameState[6]) {
+        if (gameState[0] === "X")
+            return "X";
+        else if (gameState[0] === "O") return "O";
+    }
+    if (gameState[2] === gameState[5] && gameState[2] === gameState[8]) {
+        if (gameState[2] === "X")
+            return "X";
+        else if (gameState[2] === "O") return "O";
+    }
+    if (gameState[6] === gameState[7] && gameState[6] === gameState[8]) {
+        if (gameState[6] === "X")
+            return "X";
+        else if (gameState[6] === "O") return "O";
+    }
+    if (gameState[0] === gameState[4] && gameState[0] === gameState[8]) {
+        if (gameState[0] === "X")
+            return "X";
+        else if (gameState[0] === "O") return "O";
+    }
+    if (gameState[2] === gameState[4] && gameState[2] === gameState[6]) {
+        if (gameState[2] === "X")
+            return "X";
+        else if (gameState[2] === "O") return "O";
+    }
+    if (gameState[3] === gameState[4] && gameState[3] === gameState[5]) {
+        if (gameState[3] === "X")
+            return "X";
+        else if (gameState[3] === "O") return "O";
+    }
+    if (gameState[1] === gameState[4] && gameState[1] === gameState[7]) {
+        if (gameState[1] === "X")
+            return "X";
+        else if (gameState[1] === "O") return "O";
+    }
+    return false;
 }
